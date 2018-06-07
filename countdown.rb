48e2b6e1281a64460df8615048b6b467cceea057
Wendy Calderon <wendycalderon@gmail.com>
2018-06-07 20:05:06 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+countdown_with_sleep (int)
+  sleep(int)
+end 
 
-def countdown
+
+def countdown (seconds)
+  while seconds != 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1 
+    countdown_with_sleep (1)
+  end 
+  "HAPPY NEW YEAR!"
 end
